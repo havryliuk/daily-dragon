cc0831a9a2a8213fd52e75f335935909ed8cfc7a
Oleksandr Havryliuk <oleksander.havryliuk@gmail.com>
2025-04-08 16:20:40 +0300

    #35 implement add word, list vocabulary, delete word

---
+++ daily_dragon/service/vocabulary_service.py
@@ -0,0 +1,24 @@
+import logging
+
+from fastapi import Depends
+
+from daily_dragon.repository.vocabulary_repository import VocabularyRepository
+
+
+class VocabularyService:
+
+    def __init__(self, vocabulary_repository: VocabularyRepository = Depends()):
+        self.vocabulary_repository = vocabulary_repository
+
+    def add_word(self, word):
+        return self.vocabulary_repository.add_word(word)
+
+    def get_vocabulary(self):
+        return self.vocabulary_repository.get_vocabulary()
+
+    def delete_word(self, word):
+        vocabulary = self.vocabulary_repository.get_vocabulary()
+        if word in vocabulary:
+            del vocabulary[word]
+            self.vocabulary_repository.save_vocab(vocabulary)
+        logging.info(f"Deleted word {word}")
