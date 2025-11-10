243d23fd1b3db9255d0207de8ccf9d0652414522
Oleksandr Havryliuk <oleksander.havryliuk@gmail.com>
2025-11-10 20:16:02 +0200

    #58 cleanup

--- ui/src/components/practice/PracticePage.jsx
+++ ui/src/components/practice/PracticePage.jsx
@@ -1,12 +1,10 @@
 import {Button, Text} from "@chakra-ui/react";
 
 export default function PracticePage({onReview}) {
-
-
-
     return (
         <>
-            <Text>Translate the following sentences into Chinese using the correct translation of the underlined word.</Text>
+            <Text>Translate the following sentences into Chinese using the correct translation of the underlined
+                word.</Text>
             <Button colorPalette="blue" variant="subtle" onClick={onReview}>Go to Review</Button>
         </>
     )
